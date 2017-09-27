db4219bdfb85a9a9f78feb80d8fb79141b2f8f42
Forest Newark <forest.newark@gmail.com>
2017-09-27 11:35:52 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 $( document ).ready(function() {
+//Leap End Date
+var date = new Date(2018, 1, 15);
 
-var date = new Date(2017, 4, 19);
- var now = new Date();
- var diff = (date.getTime()/1000) - (now.getTime()/1000);
+//Current Date
+var now = new Date();
+
+//Calculate time remaining
+var diff = (date.getTime()/1000) - (now.getTime()/1000);
 
 
 var clock = $('.clock').FlipClock(diff, {
@@ -12,21 +16,26 @@ var clock = $('.clock').FlipClock(diff, {
 
         var elem = document.getElementById("myBar");
         var percent = document.getElementById("percentage")
-        var tiyStartDate = new Date(2017,1,27);
-        var tiyEndDate = new Date(2017, 4, 19);
-        var totaltime = (tiyEndDate.getTime()/1000) - (tiyStartDate.getTime()/1000);
+        
+		
+		var leapStartDate = new Date(2017,8,11);
+        var leapEndDate = new Date(2018, 1, 15);
+        
+		
+		var totaltime = (leapEndDate.getTime()/1000) - (leapStartDate.getTime()/1000);
         var currentDate = new Date();
-        var timeLeft = tiyEndDate.getTime()/1000 - currentDate.getTime()/1000;
+        var timeLeft = leapEndDate.getTime()/1000 - currentDate.getTime()/1000;
         var timeElapsed = totaltime - timeLeft;
         var percentElapsed = timeElapsed / totaltime;
         var width = Math.floor(percentElapsed * 100);
         var id = setInterval(frame, 100);
-        function frame() {
+        
+		function frame() {
             if (width >= 100) {
                 clearInterval(id);
             } else {
                 elem.style.width = width + '%';
-                percent.innerHTML = width +'% done with Iron Yard';
+                percent.innerHTML = width +'% complete with LEAP';
             }
         };
 
@@ -39,8 +48,6 @@ var clock = $('.clock').FlipClock(diff, {
         secondsElapsedNow.innerHTML = secondsElaspedCount;
         secondsElaspedCount++;
       }
-
-
 
 
 
